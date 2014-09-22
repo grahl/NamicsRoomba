60e16a642fe0c985b6c5bce003decd245ef0d207
Hendrik Grahl <hendrik.grahl@namics.com>
2014-09-22 13:09:26 +0200

    Begins project with external library example program

---
+++ juppFanfare.ino
@@ -0,0 +1,73 @@
+/* RoombaLib Nov 2012 - Ralf Stoffels
+ * 
+ * Fanfare
+*/
+
+#include <roombaRS.h>
+#include <SoftwareSerial.h>
+roombaRS jupp;
+
+void setup() { 
+ 
+  jupp.wakeUp();
+  
+  jupp.goDistance(200);
+  jupp.turnRight(90);
+  jupp.turnLeft(90);
+  jupp.goBackward();
+  delay(1000);
+  jupp.stopMoving();
+  //jupp.fanfare();  
+  jupp.note(G,3,4);
+  jupp.note(G,3,4);
+  jupp.note(G,3,8);
+  jupp.note(A,3,8);
+  jupp.note(G,3,8);
+  jupp.note(F,3,8);
+  jupp.note(E,3,4);
+  jupp.note(E,3,4);
+  jupp.note(E,3,4);
+  jupp.note(P_,3,4);
+  jupp.note(F,3,4);
+  jupp.note(F,3,4);
+  jupp.note(F,3,8);
+  jupp.note(G,3,8);
+  jupp.note(F,3,8);
+  jupp.note(E,3,8);
+  jupp.note(D,3,4);
+  jupp.note(D,3,4);
+  jupp.note(D,3,4);
+  jupp.note(P_,3,4);
+  jupp.note(C,3,4);
+  jupp.note(D,3,4);
+  jupp.note(E,3,4);
+  jupp.note(F,3,4);
+  jupp.note(G,3,8);
+  jupp.note(A,3,8);
+  jupp.note(G,3,8);
+  jupp.note(A,3,8);
+  jupp.note(G,3,2);
+  jupp.note(C,4,4);
+  jupp.note(G,3,4);
+  jupp.note(G,3,8);
+  jupp.note(A,3,8);
+  jupp.note(G,3,8);
+  jupp.note(F,3,8);
+  jupp.note(E,3,4);
+  jupp.note(D,3,4);
+  jupp.note(G,3,2);
+  jupp.note(C,4,4);
+  jupp.note(G,3,4);
+  jupp.note(G,3,8);
+  jupp.note(A,3,8);
+  jupp.note(G,3,8);
+  jupp.note(F,3,8);
+  jupp.note(E,3,4);
+  jupp.note(D,3,4);
+  jupp.note(C,3,4);
+  jupp.Sleep();
+} 
+
+void loop() {
+}
+
